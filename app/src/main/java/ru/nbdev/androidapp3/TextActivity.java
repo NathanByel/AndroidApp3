6226bdc0c002e0f6a3f536e7ee4b3b3bcc0f54e8
trizet86 <n-k86@mail.ru>
2019-04-26 13:12:27 +0300

    Naming fix

--- app/src/main/java/ru/nbdev/androidapp3/TextActivity.java
+++ app/src/main/java/ru/nbdev/androidapp3/TextActivity.java
@@ -2,9 +2,6 @@ package ru.nbdev.androidapp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -21,12 +18,12 @@ public class TextActivity extends AppCompatActivity {
         TextInputEditText textInputEditText1 = findViewById(R.id.text_input_edit_text1);
 
         textInputEditText1.setOnEditorActionListener((v, actionId, event) -> {
-            textInputLayout1.setError(getString(R.string.ERROR_TEXT));
+            textInputLayout1.setError(getString(R.string.error_text));
             return false;
         });
 
         textInputEditText1.setOnKeyListener((v, keyCode, event) -> {
-            textInputLayout1.setError(getString(R.string.EMPTY_TEXT));
+            textInputLayout1.setError(getString(R.string.empty_text));
             return false;
         });
     }
