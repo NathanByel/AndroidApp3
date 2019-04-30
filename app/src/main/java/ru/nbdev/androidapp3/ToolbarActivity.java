78a69d05fd5b48c015088c6f7aa50fc018183cdc
Natan <n-k86@mail.ru>
2019-04-30 12:44:19 +0300

    hw 3

--- app/src/main/java/ru/nbdev/androidapp3/ToolbarActivity.java
+++ app/src/main/java/ru/nbdev/androidapp3/ToolbarActivity.java
@@ -1,8 +1,10 @@
 package ru.nbdev.androidapp3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ToolbarActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class ToolbarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                System.out.println("Back pressed");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
